d47564c30c1d71a3f93e86aae7762307d740ba17
Jose Raul Alfaro Guillen <jralfarog@gmail.com>
2017-10-09 23:49:33 -0600

    alphaVeqf

--- www/js/app.js
+++ www/js/app.js
@@ -281,7 +281,10 @@ app.factory('peligroFactory', function($rootScope, $localstorage, $ionicLoading,
 
 			var montar = function(){
 
+
+
 				if( window.plugins && window.plugins.NativeAudio ) {
+
 	window.plugins.NativeAudio.preloadComplex( 'music', 'audio/alarma.mp3', 1, 1, 0, function(msg){
 				console.log('ok');
 				console.log(msg);
@@ -302,6 +305,7 @@ app.factory('peligroFactory', function($rootScope, $localstorage, $ionicLoading,
 
 		paraAlerta = true;
 		window.plugins.NativeAudio.stop('music');
+		window.plugins.NativeAudio.unload( 'music' );
 		console.log('parar alarma');
 		montar();
 		}
